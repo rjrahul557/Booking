0e50ca976d9d1226b9de5d5ff4c0739fde796fc4
Rahul <rahulrjz600@gmail.com>
2024-07-02 01:23:03 +0530

    created booking create api and http communication

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -13,7 +13,7 @@ class BookingService {
     async createBooking(data){
         try{
             const flightId = data.flightId;
-            let getFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
+            const getFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
             const response = await axios.get(getFlightRequestURL);
             const flightData = response.data.data;
             let priceOfTheFlight = flightData.price;
@@ -24,6 +24,11 @@ class BookingService {
             const totalCost = priceOfTheFlight * data.noOfSeats;
             const bookingPayload = {...data,totalCost};
             const booking = await this.bookingRepository.create(bookingPayload);
+            const updateFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${booking.flightId}`;
+            console.log(updateFlightRequestURL);
+            await axios.patch(updateFlightRequestURL,{totalSeats : flightData.totalSeats-booking.noOfSeats});
+            const finalBooking = await this.bookingRepository.update(booking.id,{status : "Booked"});
+            return finalBooking;
         }
         catch(error){
             if(error.name == 'RepositoryError' || error.name == 'ValidationError'){
